3578eba1be1764e29ddaf1bfae35270a7973ee52
Raymond Buckley <raymond.ray3k@gmail.com>
2019-01-08 06:43:09 -0800

    Cleaned up default project.

--- Particle Park Code/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ Particle Park Code/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -5,8 +5,8 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new MyGdxGame(), config);
+    }
 }
