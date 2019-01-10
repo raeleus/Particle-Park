0d29ac1e4a867eb5fee9d92ebede052c0b1b4749
Raymond Buckley <raymond.ray3k@gmail.com>
2019-01-10 00:46:33 -0800

    Create PlayList.java

---
+++ Particle Park Code/core/src/com/ray3k/particlepark/PlayList.java
@@ -0,0 +1,69 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2019 Raymond Buckley.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.ray3k.particlepark;
+
+import com.badlogic.gdx.utils.Array;
+
+/**
+ *
+ * @author Raymond
+ */
+public class PlayList<T> {
+    private Array<T> list;
+    private int index;
+
+    public PlayList() {
+        list = new Array<T>();
+        index = 0;
+    }
+    
+    public void addAll(T... items) {
+        list.addAll(items);
+    }
+    
+    public void add(T item) {
+        list.add(item);
+    }
+    
+    public T get(int index) {
+        index %= list.size;
+        return list.get(index >= 0 ? index : list.size + index);
+    }
+    
+    public T next() {
+        return get(++index);
+    }
+    
+    public T previous() {
+        return get(--index);
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+}
